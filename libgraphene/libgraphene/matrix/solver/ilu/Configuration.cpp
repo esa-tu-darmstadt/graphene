4e7085973d3df8603d017e231e834887156b97c6
Tim Noack <tim@noack.id>
2024-08-02 00:15:45 +0200

    fix: Add missing load of diagonalBased field from JSON

--- libgraphene/libgraphene/matrix/solver/ilu/Configuration.cpp
+++ libgraphene/libgraphene/matrix/solver/ilu/Configuration.cpp
@@ -9,6 +9,7 @@ Configuration::Configuration(nlohmann::json const& config) {
   setFieldFromJSON<MultiColorMode>(config, "solveMulticolor", solveMulticolor);
   setFieldFromJSON<MultiColorMode>(config, "factorizeMulticolor",
                                    factorizeMulticolor);
+  setFieldFromJSON<bool>(config, "diagonalBased", diagonalBased);
 }
 
 }  // namespace graphene::matrix::solver::ilu
